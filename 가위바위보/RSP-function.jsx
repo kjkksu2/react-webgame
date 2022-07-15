191ff51b7758873dd824569aa230e2cc02134bb7
Kim Seok Yoon <kjkksu2@naver.com>
2022-07-15 19:09:58 +0900

    가위바위보/custom hook

--- 가위바위보/RSP-function.jsx
+++ 가위바위보/RSP-function.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef, useEffect } from "react";
+import useInterval from "./useInterval";
 
 const rspCoords = {
   바위: "0",
@@ -20,19 +21,7 @@ const RSP = () => {
   const [result, setResult] = useState("");
   const [imgCoord, setImgCoord] = useState(rspCoords.바위);
   const [score, setScore] = useState(0);
-
-  const interval = useRef();
-
-  // componentDidMount, componentDidUpdate 역할(1대1 대응은 아님)
-  useEffect(() => {
-    console.log("다시 실행");
-    interval.current = setInterval(changeHand, 100);
-    return () => {
-      // componentWillUnmount 역할
-      console.log("종료");
-      clearInterval(interval.current);
-    };
-  }, [imgCoord]);
+  const [isRunning, setIsRunning] = useState(true);
 
   const changeHand = () => {
     if (imgCoord === rspCoords.바위) {
@@ -44,26 +33,30 @@ const RSP = () => {
     }
   };
 
+  useInterval(changeHand, isRunning ? 100 : null);
+
   const onClickBtn = (choice) => () => {
-    clearInterval(interval.current);
+    if (isRunning) {
+      setIsRunning(false);
 
-    const myScore = scores[choice];
-    const cpuScore = scores[computerChoice(imgCoord)];
-    const diff = myScore - cpuScore;
+      const myScore = scores[choice];
+      const cpuScore = scores[computerChoice(imgCoord)];
+      const diff = myScore - cpuScore;
 
-    if (diff === 0) {
-      setResult("비겼습니다!");
-    } else if ([-1, 2].includes(diff)) {
-      setResult("이겼습니다!");
-      setScore((prevScore) => prevScore + 1);
-    } else {
-      setResult("졌습니다!");
-      setScore((prevScore) => prevScore - 1);
+      if (diff === 0) {
+        setResult("비겼습니다!");
+      } else if ([-1, 2].includes(diff)) {
+        setResult("이겼습니다!");
+        setScore((prevScore) => prevScore + 1);
+      } else {
+        setResult("졌습니다!");
+        setScore((prevScore) => prevScore - 1);
+      }
+
+      setTimeout(() => {
+        setIsRunning(true);
+      }, 1000);
     }
-
-    setTimeout(() => {
-      interval.current = setInterval(changeHand, 100);
-    }, 1000);
   };
 
   return (
